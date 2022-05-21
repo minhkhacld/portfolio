334399dec45319b9fc8e827f650b8d508f9701b6
Kha Pham Minh <bitg.khapham@pdcbodynits.com>
2022-05-21 08:17:25 +0700

    add

--- src/components/page/skill/range/SkillRange.js
+++ src/components/page/skill/range/SkillRange.js
@@ -55,16 +55,26 @@ const SkillRange = () => {
                     <RowLeft className="exp-title">Software Developer</RowLeft>
                     <RowLeft className="exp-company">Bodynits Tien Giang</RowLeft>
                     <RowLeft className="exp-time">2021-Now</RowLeft>
-                    <p className="exp-description">Working as a Front End developer, built and deploy the dynamic web applications to control and improve manufacturing processes. Maintain and continous improve the applications base on the need.</p>
+                    <p className="exp-description">- Participate in analysis, design and product development using ReactJS, NodeJS.
+                    </p>
+                    <p className="exp-description">- Build high performance, scalable components for company websites.</p>
+                    <p className="exp-description">- Participate in designing and building application.</p>
+                    <p className="exp-description">- Develop new user-facing features using JavaScript and ReactJS that meet large number of users.</p>
+                    <p className="exp-description">- Build reusable components and front-end libraries for future use.</p>
+                    <p className="exp-description">- Follow coding standards, write automated test cases, deployment scripts.</p>
+                    <p className="exp-description">- Assist in improving app architectures – Explore existing systems, determines areas of maintainability scalability extensibility.</p>
                 </Col>
                 <Col className="exp-job">
                     <RowLeft className="exp-title">Industrial Engineer</RowLeft>
                     <RowLeft className="exp-company">Hansae TG - Premier Global VN - Techtronic Industry</RowLeft>
                     <RowLeft className="exp-time">2015-2021</RowLeft>
-                    <p className="exp-description">Run engineering build & pilot production to build new samples. Define manpower, output and ST BOM of new products.
-                        ,Create Work instruction, Layout, Equipment arrangement, Work flow for Mass production, train production team how to build product, control WIP.
-                        ,Execute time study & improve LOB for production line, continuously do improvement projects to increase efficiency & save the cost.
-                        With  <strong style={{ color: 'var(--lightBlue)' }}>ambitious</strong> to improve manufacturing processes. I have <strong style={{ color: 'var(--lightBlue)' }}>learned programming</strong> and <strong style={{ color: 'var(--lightBlue)' }}>started to build</strong> small projects during this period time to support production such as: Scan machine QR code, Production output tracking, Change over tracking app...</p>
+                    <p className="exp-description">- Create Work instruction, Layout, Equipment arrangement, Work flow for Mass production, train production team how to build product, control WIP.
+                    </p>
+                    <p className="exp-description">- Run engineering build & pilot production to build new samples. Define manpower, output and ST BOM of new products.</p>
+                    <p className="exp-description">- Execute time study & improve LOB for production line, continuously do improvement projects to increase efficiency & save the cost.</p>
+                    <p className="exp-description">-  With  <strong style={{ color: 'var(--lightBlue)' }}>ambitious</strong> to improve manufacturing processes. I have <strong style={{ color: 'var(--lightBlue)' }}>learned programming</strong> and <strong style={{ color: 'var(--lightBlue)' }}>started to build</strong> small projects during this period time to support production such as: Scan machine QR code, Production output tracking, Change over tracking app...</p>
+
+
                 </Col>
             </div>
         </Col>
